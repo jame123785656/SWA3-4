c1e4be6bb0454d2f9b0eb3de5e1c7b5384d1e124
unknown <61955070+jame123785656@users.noreply.github.com>
2021-07-19 16:07:39 +0700

    Initial commit

---
+++ src/Honda.java
@@ -0,0 +1,11 @@
+  
+
+public class Honda extends Car {
+	public void HondaStart(){
+
+        CarEngine Honda_Engine = new CarEngine(); //composition
+
+        Honda_Engine.stopEngine();
+
+        }
+}
